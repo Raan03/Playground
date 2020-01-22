c8dc43c572cd4cf5d4c7ba711e3d8d4e6baef440
Raan03 <info@raan03.be>
2020-01-22 19:44:05 +0100

    Add Template form

--- Angular/Forms/src/app/app.module.ts
+++ Angular/Forms/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
