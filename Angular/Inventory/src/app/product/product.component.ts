36f3e157d2b50ed0577b0728eced57e78419a4ea
Raan03 <info@raan03.be>
2020-01-22 17:19:11 +0100

    Fix typo

--- Angular/Inventory/src/app/product/product.component.ts
+++ Angular/Inventory/src/app/product/product.component.ts
@@ -7,7 +7,7 @@ import { Product } from './product.model';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {  
-	products: Product;
+	product: Product;
   constructor() { 
 	this.product =	new Product(
 		'AB01 (SKU)', 'YOLANDA (NAME)', 'Google.com (URL)', [ 'D (DEP1)', 'D (DEP2)'], 13.37
