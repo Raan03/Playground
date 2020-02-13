dca605de244ce44b9207164894b7b634998eb73c
Raan03 <info@raan03.be>
2020-02-13 16:46:58 +0100

    Adding barebone components & service

--- Angular/MusicSearch/src/app/app.module.ts
+++ Angular/MusicSearch/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { SearchComponent } from './search/search.component';
+import { ArtistComponent } from './artist/artist.component';
+import { TrackComponent } from './track/track.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    SearchComponent,
+    ArtistComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule
