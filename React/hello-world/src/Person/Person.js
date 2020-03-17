5906290379c61308bdb3592d5935cb930db0e3e1
Raan03 <info@raan03.be>
2020-03-17 13:01:45 +0100

    adding props children

--- React/hello-world/src/Person/Person.js
+++ React/hello-world/src/Person/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 // JavaScript source code
 const person = (props) => {
-    return <p>I'm a person called {props.name} and I am {props.age} years old</p>
+    return (
+        <div className="personCard">
+            <p>I'm a person called {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+        )
 }
 
 export default person;
