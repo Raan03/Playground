b3f3bc84f5bda664cc4826c34b19e77486e990ca
Raan03 <info@raan03.be>
2020-03-17 13:26:15 +0100

    add clickHandler

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -6,12 +6,16 @@ class App extends Component {
     state = {
         persons: [{ name: "Ranu", age: 31 }, { name: "Lucy", age: 10}]
     }
-    //return React.createElement('div', { className: "App" }, React.createElement("h1", null, "It works with createElements"));
+
+    switchNameHandler = () => {
+        console.log("We clicked");
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>It works!</h1>
-                <button>Switch Names</button>
+                <button onClick={this.switchNameHandler}>Switch Names</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>This is my dog</Person>
             </div>
