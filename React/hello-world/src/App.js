4c838b97b5173c1fab3090bb0171c3918af1f3ce
Raan03 <info@raan03.be>
2020-03-17 12:40:39 +0100

    adding person component

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
     //return React.createElement('div', { className: "App" }, React.createElement("h1", null, "It works with createElements"));
   return (
       <div className="App">
           <h1>It works!</h1>
+          <Person />
     </div>
   );
 }
---
+++ React/hello-world/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// JavaScript source code
+const person = () => {
+    return <p>I'm a person</p>
+}
+
+export default person;
