fb3ff019c16c7d81d7232a91330a6e67250d6b10
Raan03 <info@raan03.be>
2020-03-17 15:10:42 +0100

    fine tune css

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -27,7 +27,8 @@ class App extends Component {
             backgroundColor: 'white',
             font: 'inherit',
             border: '1px solid blue',
-            padding: '8px'
+            padding: '8px',
+            cursor: 'pointer'
         };
 
         return (
