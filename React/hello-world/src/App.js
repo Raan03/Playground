46552ed3c95f6b3d5c20c12e9865f3f2590c6228
Raan03 <info@raan03.be>
2020-03-17 14:35:45 +0100

    Revert "switch back to function App"
    This reverts commit f3275ecba16fdb0197c42b475651da6eaec8dfcc.

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props =>
-{
-    const [ personsState, setPersonsState] = useState({
+class App extends Component {
+    state = {
         persons: [{ name: "Ranu", age: 31 }, { name: "Lucy", age: 10 }],
         otherState: 'some other value'
-    });
+    }
 
-const switchNameHandler = () => {
-    console.log("We clicked");
-    // don't do this: this.state.persons[0].name = "Ranu Annaert";
-    console.log(personsState);
-    setPersonsState({ persons: [{ name: "Ranu Annaert", age: 32 }, { name: "Lucy Annaert", age: 10 }] });
-    console.log(personsState);
-}
+    switchNameHandler = () => {
+        console.log("We clicked");
+        // don't do this: this.state.persons[0].name = "Ranu Annaert";
+        console.log(this.state);
+        this.setState({ persons: [{ name: "Ranu Annaert", age: 32 }, { name: "Lucy Annaert", age: 10 }] });
+        console.log(this.state);
+    }
+
+    render() {
         return (
             <div className="App">
                 <h1>It works!</h1>
-                <button onClick={switchNameHandler}>Switch Names</button>
-                <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-                <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>This is my dog</Person>
+                <button onClick={this.switchNameHandler}>Switch Names</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>This is my dog</Person>
             </div>
         );
+    }
 }
 
 export default App;
