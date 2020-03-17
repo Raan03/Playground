f7e56f68d89c45ce8e9593d208b105cfa0beb697
Raan03 <info@raan03.be>
2020-03-17 12:29:12 +0100

    restore back to HTML

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return React.createElement('div', { className: "App" }, React.createElement("h1", null, "It works with createElements"));
-  /*return (
+    //return React.createElement('div', { className: "App" }, React.createElement("h1", null, "It works with createElements"));
+  return (
       <div className="App">
           <h1>It works!</h1>
     </div>
-  );*/
+  );
 }
 
 export default App;
