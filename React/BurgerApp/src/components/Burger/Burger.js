d3134b9b08260f02503e1ef6c65ca676b4347231
Raan03 <info@raan03.be>
2020-03-18 18:57:04 +0100

    typo

--- React/BurgerApp/src/components/Burger/Burger.js
+++ React/BurgerApp/src/components/Burger/Burger.js
@@ -6,7 +6,7 @@ const burger = (props) => {
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="salad"></BurgerIngredient>
+            {/* <BurgerIngredient type="salad"></BurgerIngredient> */}
             <BurgerIngredient type="cheese"></BurgerIngredient>
             <BurgerIngredient type="meat"></BurgerIngredient>
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
