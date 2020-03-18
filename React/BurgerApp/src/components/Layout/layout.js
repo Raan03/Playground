b9f76683ca5f856586caa8f26e4cae92eb183457
Raan03 <info@raan03.be>
2020-03-18 18:15:23 +0100

    BurgerBuilder component

--- React/BurgerApp/src/components/Layout/layout.js
+++ React/BurgerApp/src/components/Layout/layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Auxilary';
+import styles from './layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
-    </Aux>
+    </Aux >
 );
 
 export default layout;
