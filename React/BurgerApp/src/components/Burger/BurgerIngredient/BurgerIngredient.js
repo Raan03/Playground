7d0e0bcf52185b74bf1b480b3cfb91432e3eebce
Raan03 <info@raan03.be>
2020-03-18 18:54:36 +0100

    add css modules

--- React/BurgerApp/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ React/BurgerApp/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from './burgerIngredient.module.css';
+import styles from './BurgerIngredient.module.css';
 
 import PropTypes from 'prop-types';
 
