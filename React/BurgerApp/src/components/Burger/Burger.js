00e0ba77dd89990355f5f15efdae58ece368719b
Raan03 <info@raan03.be>
2020-03-19 09:41:03 +0100

    handle Empty Ingredients

--- React/BurgerApp/src/components/Burger/Burger.js
+++ React/BurgerApp/src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(key => {
+    let transformedIngredients = Object.keys(props.ingredients).map(key => {
         return [...Array(props.ingredients[key])].map((_, i) => {
             return <BurgerIngredient key={key + i} type={key} />
         })
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
+    console.log(transformedIngredients);
 
     return (
         <div className={styles.Burger}>
