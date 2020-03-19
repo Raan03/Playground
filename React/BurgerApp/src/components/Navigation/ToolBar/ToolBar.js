ffd412dd49ecd9822d808ee93be018adad6a7808
Raan03 <info@raan03.be>
2020-03-19 13:21:45 +0100

    add navigationitems

--- React/BurgerApp/src/components/Navigation/ToolBar/ToolBar.js
+++ React/BurgerApp/src/components/Navigation/ToolBar/ToolBar.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import classes from './ToolBar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const ToolBar = (props) => (
     <header className={classes.ToolBar}>
         <div>MENU</div>
         <Logo />
-        <div>LOGO</div>
         <nav>
-            <ul>
-                <li>
-                    L1
-        </li>
-                <li>
-                    L2
-        </li>
-            </ul>
+            <NavigationItems></NavigationItems>
         </nav>
-    </header>
+    </header >
 );
 
 export default ToolBar;
