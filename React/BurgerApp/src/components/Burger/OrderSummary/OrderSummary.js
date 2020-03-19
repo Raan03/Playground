2c08707be31540a256f56b88ffb937cf997215ce
Raan03 <info@raan03.be>
2020-03-19 14:58:56 +0100

    perfupdates

--- React/BurgerApp/src/components/Burger/OrderSummary/OrderSummary.js
+++ React/BurgerApp/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,42 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxilary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>
-                        {igKey}
-                    </span>
-                : {props.ingredients[igKey]}
-                </li>
-            )
-        });
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log("OrderSummary updates");
+    }
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>
+                            {igKey}
+                        </span>
+                : {this.props.ingredients[igKey]}
+                    </li>
+                )
+            });
 
-    return (<Aux>
-        <h3>Your order</h3>
-        <p>Selected ingredients: </p>
-        <ul>
-            {ingredientsSummary}
-        </ul>
-        <p><strong>Total price: € {props.totalprice}</strong></p>
-        <p>Order?</p>
-        <Button
-            clicked={props.cancelButton}
-            btnType="Danger"
-        >
-            No
+        return (<Aux>
+            <h3>Your order</h3>
+            <p>Selected ingredients: </p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p><strong>Total price: € {this.props.totalprice}</strong></p>
+            <p>Order?</p>
+            <Button
+                clicked={this.props.cancelButton}
+                btnType="Danger"
+            >
+                No
             </Button>
-        <Button
-            clicked={props.confirmButton}
-            btnType="Success"
-        >
-            Yes
+            <Button
+                clicked={this.props.confirmButton}
+                btnType="Success"
+            >
+                Yes
         </Button>
-    </Aux>)
+        </Aux >)
+    }
 }
 
-export default OrderSummary
-    ;
+export default OrderSummary;
--- React/BurgerApp/src/components/UI/Modal/Modal.js
+++ React/BurgerApp/src/components/UI/Modal/Modal.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxilary';
 import BackDrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => (
-    <Aux>
-        <BackDrop showModal={props.showModal} clicked={props.modalClosed}></BackDrop>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.showModal ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.showModal !== this.props.showModal;
+    }
+    componentWillUpdate() {
+        console.log("Modal will update");
+    }
+    render() {
+        return (<Aux>
+            <BackDrop showModal={this.props.showModal} clicked={this.props.modalClosed}></BackDrop>
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: this.props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.showModal ? '1' : '0'
+                }}
+            >
+                {this.props.children}
+            </div>
 
-    </Aux>
-);
-
+        </Aux>
+        );
+    }
+}
 export default Modal;
