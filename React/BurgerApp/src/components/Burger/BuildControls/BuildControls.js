82a77326da1624029f4396a33db9946b7a7fc031
Raan03 <info@raan03.be>
2020-03-19 10:05:03 +0100

    add css styling, buildcontrols

---
+++ React/BurgerApp/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
+
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <BuildControl></BuildControl>
+    </div>
+
+);
+
+export default buildControls;
