7088de0654f0b287e8ebab66c6da4eaa47eedaf3
Raan03 <info@raan03.be>
2020-03-20 11:35:25 +0100

    make 2nd call await result

--- Angular/Udemy/DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ Angular/Udemy/DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -31,7 +31,7 @@ namespace DatingApp.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefaultAsync(d => d.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(d => d.Id == id);
 
             return Ok(value);
         }
