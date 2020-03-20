c25c23a19e27e6972d253cdd85c631310ec6430d
Raan03 <info@raan03.be>
2020-03-20 11:30:34 +0100

    Add DBMigration

--- Angular/Udemy/DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ Angular/Udemy/DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -1,27 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
 namespace DatingApp.API.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DatingApp.API.Data;
+    using DatingApp.API.Models;
+    using Microsoft.AspNetCore.Mvc;
+
     [ApiController]
     [Route("api/[controller]")]
     public class ValuesController : ControllerBase
     {
+        protected readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _context.Values.FirstOrDefault(d => d.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
