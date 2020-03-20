e488c93e3a23e5d0ea3143f89cfb50e375b79a45
Raan03 <info@raan03.be>
2020-03-20 18:53:15 +0100

    add home & registercomponents

--- Angular/Udemy/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ Angular/Udemy/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,4 +21,13 @@ export class NavComponent implements OnInit {
       }
     );
   }
+
+  loggedIn() {
+    const token = localStorage.getItem("token");
+    return !!token;
+  }
+  logOut() {
+    localStorage.removeItem("token");
+    console.log("logged out");
+  }
 }
