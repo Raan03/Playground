0df021ac8e5a9a7f056c1df9efad6c9eef5596c9
Raan03 <info@raan03.be>
2020-03-20 14:21:42 +0100

    Add ModelValidation Min,MaxLength for PSW

--- Angular/Udemy/DatingApp/DatingApp.API/DTO/UserForRegisterDto.cs
+++ Angular/Udemy/DatingApp/DatingApp.API/DTO/UserForRegisterDto.cs
@@ -8,6 +8,7 @@ namespace DatingApp.API.DTO
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(maximumLength: 8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4-8 characters.")]
         public string Password { get; set; }
     }
 }
