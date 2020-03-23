fe0704d1f173155f1b4446df7c1beb9c79f3d651
Raan03 <info@raan03.be>
2020-03-23 18:19:53 +0100

    add member card

--- Angular/Udemy/DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ Angular/Udemy/DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -14,10 +14,11 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
-import { MemberListComponent } from "./member-list/member-list.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./members/member-list/member-card/member-card.component";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { appRoutes } from "./routes";
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
