4fbeb0efa90dcc06d6939fa65fb6ff3da7d491d2
Raan03 <info@raan03.be>
2020-03-24 09:35:33 +0100

    add ngs gallery crap

--- Angular/Udemy/DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ Angular/Udemy/DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { appRoutes } from "./routes";
 import { MemberCardComponent } from "./members/member-card/member-card.component";
 import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -55,7 +56,12 @@ export function tokenGetter() {
     }),
     TabsModule.forRoot()
   ],
-  providers: [AuthService, ErrorInterceptorProvider, MemberDetailResolver],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    MemberDetailResolver,
+    MemberListResolver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
