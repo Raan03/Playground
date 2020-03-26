28e8a7407f6480d8755748d67ceb67eb299969e5
Raan03 <info@raan03.be>
2020-03-26 13:45:20 +0100

    add pagination to Angular

--- Angular/Udemy/DatingApp/DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ Angular/Udemy/DatingApp/DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,13 +8,16 @@ import { Observable, of } from "rxjs";
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
+
   constructor(
     private userService: UserService,
     private alertifyService: AlertifyService,
     private router: Router
   ) {}
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers().pipe(
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
       catchError(error => {
         this.alertifyService.message("Problem retrieving data");
         this.alertifyService.error(error);
