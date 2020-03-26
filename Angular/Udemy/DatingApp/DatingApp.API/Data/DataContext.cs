bc7058990b176432b4814d4cbbae64f6cb352c62
Raan03 <info@raan03.be>
2020-03-26 15:39:26 +0100

    Add LikesTable + entity

--- Angular/Udemy/DatingApp/DatingApp.API/Data/DataContext.cs
+++ Angular/Udemy/DatingApp/DatingApp.API/Data/DataContext.cs
@@ -11,8 +11,28 @@ namespace DatingApp.API.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Like>()
+            .HasKey(k => new { k.LikerId, k.LikeeId });
+
+            modelBuilder.Entity<Like>()
+            .HasOne(d => d.Likee)
+            .WithMany(u => u.Likers)
+            .HasForeignKey(u => u.LikeeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Like>()
+            .HasOne(d => d.Liker)
+            .WithMany(d => d.Likees)
+            .HasForeignKey(u => u.LikerId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
     }
 }
