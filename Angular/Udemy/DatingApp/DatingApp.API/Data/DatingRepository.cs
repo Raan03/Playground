f75177e3ae0cf87efad78551255cf45953efe666
Raan03 <info@raan03.be>
2020-03-26 16:01:40 +0100

    Api to filter likers/likees

--- Angular/Udemy/DatingApp/DatingApp.API/Data/DatingRepository.cs
+++ Angular/Udemy/DatingApp/DatingApp.API/Data/DatingRepository.cs
@@ -52,6 +52,19 @@ namespace DatingApp.API.Data
             users = users.Where(d => d.Id != userParams.UserId);
             users = users.Where(d => d.Gender == userParams.Gender);
 
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
+
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                 var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
@@ -74,6 +87,18 @@ namespace DatingApp.API.Data
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _dataContext.Users.Include(u => u.Likers).Include(f => f.Likees).FirstOrDefaultAsync(u => u.Id == id);
+
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id).Select(d => d.LikerId);
+            }
+
+            return user.Likers.Where(u => u.LikerId == id).Select(d => d.LikeeId);
         }
         public async Task<Like> GetLike(int userId, int recipientId)
         {
